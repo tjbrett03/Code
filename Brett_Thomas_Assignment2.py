852cc366dde3143e72e141d189be7738bfebf0c2
Thomas Brett <32444844+tjbrett03@users.noreply.github.com>
2022-09-08 15:30:16 -0500

    commit Brett_Thomas_Assignment2 commit farenheit_to_celsius

---
+++ Brett_Thomas_Assignment2.py
@@ -0,0 +1,5 @@
+length = int(input("Length: ")) # set var legnth equal to user input for length
+width = int(input("Width: ")) # set var width equal to user input for width
+area = length * width # calculate area
+perimeter = (length * 2) + (width * 2) #calculate perimeter
+print("Area:", area, "\nPerimeter:", perimeter) # display area and perimeter to user
---
+++ farenheit_to_celsius.py
@@ -0,0 +1,3 @@
+f = float(input("Fareneheit: "))
+c = (f-32)*(5/9)
+print(c, "Degrees Celsius")
