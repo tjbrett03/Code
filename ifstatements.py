a6aa4646cc8d9501f782e74c6de238c8a8766362
Thomas Brett <32444844+tjbrett03@users.noreply.github.com>
2022-09-09 14:57:01 -0500

    add name to file

--- ifstatements.py
+++ ifstatements.py
@@ -1,3 +1,4 @@
+# Thomas Brett-Labouchere 9/9/2022 CYB 267
 planets = ["earth", "saturn", "neptune", "mars"] # Create array with names of planets
 density = [1.217, 0.19, 0.45, 0.020]             # Create array with density values of planets
 accel = [9.798, 10.44, 11.15, 3.71]              # Create array with acceleration values of planets
